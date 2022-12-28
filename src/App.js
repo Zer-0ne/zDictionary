272ed8969ed49757629622e5d6b7f359041c5b7b
Zer-0ne <sahilkhan8294799@gmail.com>
2022-12-28 19:00:31 +0530

    zDictionary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import './css/fontawesome-free-6.2.1-web/css/all.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
+import { useState } from 'react';
+import Dictionary from './Component/Dictionary/Dictionary';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Dictionary/>
+    </>
   );
 }
-
 export default App;
